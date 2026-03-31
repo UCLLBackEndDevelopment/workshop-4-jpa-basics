ce94d5b2393ec93f7256b2e39f86b99b0e41e2f6
Bram <bram.vanimpe@ucll.be>
2026-03-31 14:12:53 +0200

    New start point for workshop 5

--- src/main/java/be/ucll/LibraryApplication.java
+++ src/main/java/be/ucll/LibraryApplication.java
@@ -1,14 +1,7 @@
 package be.ucll;
 
-import be.ucll.model.Book;
-import be.ucll.model.Magazine;
-import be.ucll.model.Publication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.swing.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class LibraryApplication {
